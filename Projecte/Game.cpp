78264f5f4238ef12f9f33f200e57c14e07991c84
matedavid <david.mate.lopez@gmail.com>
2023-03-30 16:26:22 +0200

    level02 with description

--- Projecte/Game.cpp
+++ Projecte/Game.cpp
@@ -23,13 +23,40 @@ static Scene::Description sceneLevel01()
 	return description;
 }
 
+static Scene::Description sceneLevel02()
+{
+	Scene::Description description{};
+
+	description.levelName = "level02.txt";
+	description.playerPositionStartTile = {4, 18};
+
+	description.skeletonDescriptions = {
+		Scene::EnemyDescription{16, 12, MOVE_LEFT},
+	};
+
+	description.vampireDescriptions = {
+		Scene::EnemyDescription{12, 6, MOVE_LEFT},
+		Scene::EnemyDescription{18, 6, MOVE_RIGHT},
+	};
+
+	description.ghostDescriptions = {
+		Scene::EnemyDescription{4, 6, MOVE_RIGHT},
+		Scene::EnemyDescription{26, 6, MOVE_LEFT},
+	};
+
+	description.keyPositionTile = {12, 18};
+	description.doorPositionTile = {20, 4};
+
+	return description;
+}
+
 Game::Game()
 {
 	Scene::Description level01 = sceneLevel01();
-	Scene::Description level02 = sceneLevel01();
+	Scene::Description level02 = sceneLevel02();
 
-	levelDescriptions.push_back(level01);
 	levelDescriptions.push_back(level02);
+	levelDescriptions.push_back(level01);
 
 	currentLevelIdx = 0;
 	currentSceneType = SceneType::MainMenu;
